7158b6042e3ce30d54d53b4ab3d56ac689ba6a58
Jordan Banasik <woovie@woovie.net>
2022-05-30 07:36:25 -0700

    Small updates
    .gitignore:
    Added ignore for deploy file and folder
    
    prepare-lambda-deploy.sh:
    Altered to make its own directory to perform prep work in as it
    was messing up my environment otherwise.

--- prepare-lambda-deploy.sh
+++ prepare-lambda-deploy.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
+mkdir -v deploy &&
+
+cp -v *.py requirements.txt deploy &&
+
+cd deploy &&
+
+mv lambda_scrape.py lambda_function.py &&
+
 PYENV_VERSION=3.9.11
 
-pip install -r requirements.txt --upgrade --target .
+pip install -r requirements.txt --upgrade --target . &&
 
 unset PYENV_VERSION
 
-gfind -type f -exec chmod -v 0644 {} \;
-gfind -type d -exec chmod -v 0755 {} \;
+gfind -type f -exec chmod -v 0644 {} \; &&
+gfind -type d -exec chmod -v 0755 {} \; &&
+
+tar -czvf ../deploy.tar.gz *

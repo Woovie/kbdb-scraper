98de830ddf3868585d23aac100cae1ed09b598d2
Jordan Banasik <woovie@woovie.net>
2022-05-31 13:02:43 -0700

    Added stores (Platform) module
    This is a simple enum for platform (e.g. Shopify) relation to an
    int for simplifying and making more accurate code and payloads.

---
+++ stores.py
@@ -0,0 +1,10 @@
+"""
+Simple Enum class as a module as its used by multiple Lambdas
+"""
+from enum import Enum, auto
+
+class Platform(Enum):
+    """
+    Simple Enum class
+    """
+    SHOPIFY = auto()
